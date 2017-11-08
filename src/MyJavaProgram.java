212633b11cc455c348bb38442926d80d7a29d455
Martis.Ben <Martis.Ben@SEMS-259-36.SEMS259.LAB>
2017-11-08 14:50:31 -0800

    RobotSquare MyJavaProject

--- src/MyJavaProgram.java
+++ src/MyJavaProgram.java
@@ -6,8 +6,15 @@ public class MyJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
+		Robot jakepaul=new Robot("vic");
+		jakepaul.setSpeed(30);
+		jakepaul.penDown();
+		for (int i = 0; i < 4 ; i++) {
+			jakepaul.move(200);
+			jakepaul.turn(90);
+			jakepaul.setRandomPenColor();
+
+		}
 		
 	}
 }
--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -11,19 +11,23 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+    	Robot yeet = new Robot("vic");
 
         // 3. Put the robot's pen down
-
+    	yeet.penDown();
 
         // 6. Make the robot move as fast as possible
-
-
+    	yeet.setSpeed(999999999);
+    	yeet.penDown();
+    	yeet.move(100);
         // 5. Do everything below here 4 times
-
+    	for (int i = 0; i < 4; i++) {
+			yeet.move(100);
+			yeet.turn(90);
+		}
 
         //         2. Move your robot 200 pixels
-
+    	
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
 
